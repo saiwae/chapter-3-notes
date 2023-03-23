a4bf7e9bb4f216eaa3607d910563fb3abb30e0e7
khloe <slowykhloe@gmail.com>
2023-03-23 10:25:29 -0700

    intial commit functions02 added 3/16 array notes + practice 3/23

---
+++ arrays1.js
@@ -0,0 +1,41 @@
+//1 & 3?
+let nums = ['3', '4', '5', '6', '7', '8', '9', '10', '11', '12'];
+console.log(nums[0]);
+
+//2
+console.log(nums[3]);
+
+//4
+console.log(nums[3]);
+console.log(nums[2]);
+console.log(nums[1]);
+console.log(nums[0]);
+
+//5
+console.log(nums[2]);
+console.log(nums[5]);
+console.log(nums[8]);
+
+//6
+console.log(nums[2]);
+console.log(nums[4]);
+console.log(nums[6]);
+console.log(nums[8]);
+
+//7
+nums.shift();
+console.log(nums);
+
+//8
+let cut = nums.slice(2, 10);
+console.log(cut);
+
+//9
+let nums2 = ['3', '3', '4', '5', '5', '5', '6', '7'];
+console.log(nums2[2]);
+console.log(nums2[6]);
+console.log(nums2[7]);
+
+//10
+console.log(nums2[0]);
+console.log(nums2[3]);
